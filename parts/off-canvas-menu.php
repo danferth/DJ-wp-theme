3a639aa19c90822f989a55fe53dba50c40384869
Dan Klotz <danferth@gmail.com>
2015-10-24 05:38:12 +0000

    tab-bar 86 text + logo, added function to remove open sans loading

--- parts/off-canvas-menu.php
+++ parts/off-canvas-menu.php
@@ -16,7 +16,7 @@
   <section class="middle tab-bar-section">
 
     <h1 class="title">
-      <?php bloginfo( 'name' ); ?>
+      <a href="<?php echo home_url(); ?>"><img src="<?php  echo get_template_directory_uri() . '/images/TIC-logo-white.png'; ?>" alt="<?php bloginfo( 'name' ); ?>"></a>
     </h1>
 
   </section>
