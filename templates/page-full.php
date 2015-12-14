5e6aa9a6c5d7dd9e576e1999bd45742e7b9e8903
Dan Klotz <danferth@gmail.com>
2015-12-14 19:39:05 +0000

    added custom attribute for ng-app and ng-controller to make things a little bit easier

--- templates/page-full.php
+++ templates/page-full.php
@@ -8,6 +8,18 @@ $prefooter_class = "has-prefooter";
 	$prefooter_class = "";
 }
 
+$ng_app = "";
+if(get_post_meta($post->ID, "ng-app")){
+$ngApp = get_post_meta($post->ID, "ng-app", true);
+$ng_app = "ng-app='" . $ngApp . "'";
+}
+
+$ng_controller = "";
+if(get_post_meta($post->ID, "ng-controller")){
+$ngController = get_post_meta($post->ID, "ng-controller", true);
+$ng_controller = "ng-controller='" . $ngController . "'";
+}
+
 
 get_header(); ?>
 
@@ -20,7 +32,7 @@ get_header(); ?>
 			<header>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 			</header>
-	<div class="small-12 large-12 columns" role="main">
+	<div class="small-12 large-12 columns" role="main" <?php echo $ng_app . " " . $ng_controller; ?>>
 
 			<div class="entry-content">
 				<?php the_content(); ?>
