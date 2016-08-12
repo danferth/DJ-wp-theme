d2471f500af5941a0b0483b70acfb329d230260c
Dan Klotz <danferth@gmail.com>
2016-08-12 22:46:54 +0000

    added close button to overlay

--- assets/javascript/directives.js
+++ assets/javascript/directives.js
@@ -9,10 +9,14 @@ plates.directive('plateSearch', function(){
     replace: 'true',
     templateUrl: url+'/wp-content/themes/TIC/assets/templates/plate-search.html',
     link: function(scope, elem, attrs){
-        elem.bind('click', function(){
-          elem.addClass('hidden');
-          elem.children('.overlay-content').removeClass('fadeInUp');
-        });
+      // elem.bind('click', function(){
+      //   elem.addClass('hidden');
+      //   elem.children('.overlay-content').removeClass('fadeInUp');
+      // });
+      elem.find('.close-overlay').bind('click', function(){
+        elem.children('.overlay-content').removeClass('fadeInUp');
+        elem.addClass('hidden');
+      });
     }
   };
 });
