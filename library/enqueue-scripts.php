f252e1cc6d98be8e8a7a9d4cee181f66ef915c17
Dan Klotz <danferth@gmail.com>
2016-11-01 22:59:43 +0000

    top search reworked, version appended to css for breaking cache on push

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -17,7 +17,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	//wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Oswald:400,700|Source+Sans+Pro:400,300,700' );
 
 	// Enqueue the main Stylesheet.
-	wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/assets/stylesheets/foundation.css' );
+	wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/assets/stylesheets/foundation.css?v=1.0.2' );
 
 	// Deregister the jquery version bundled with WordPress.
 	wp_deregister_script( 'jquery' );
@@ -52,7 +52,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	wp_enqueue_script('app', get_template_directory_uri(). '/assets/javascript/app.js', array('angular'), '1.0.1', true);
 	wp_enqueue_script('directives', get_template_directory_uri(). '/assets/javascript/directives.js', array('angular'), '1.0.1', true);
 	//site.js TIC custome js for pages
-	wp_enqueue_script('site', get_template_directory_uri() . '/assets/javascript/site.js', array('jquery'), '1.0.1', true);
+	wp_enqueue_script('site', get_template_directory_uri() . '/assets/javascript/site.js', array('jquery'), '1.0.2', true);
 
 	// If you'd like to cherry-pick the foundation components you need in your project, head over to Gruntfile.js and see lines 67-88.
 	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
--- parts/top-search.php
+++ parts/top-search.php
@@ -8,30 +8,25 @@
  */
 
 ?>
-
-<div class="extend hide-for-small-only">
-    	
-        <?php do_action( 'foundationpress_before_searchform' ); ?>
-        
+<?php do_action( 'foundationpress_before_searchform' ); ?>
 <form class="top-search-bar" role="search" method="get" id="cse-search-box" action="<?php echo home_url( '/results/' ); ?>">
-	<div class="row collapse">
-		<?php do_action( 'foundationpress_searchform_top' ); ?>
-		<div class="column small-12 medium-8 large-10">
-			<input type="hidden" name="cx" value="009032756241832010141:tvv1cneufti" />
-    		<input type="hidden" name="cof" value="FORID:10;NB:1" />
-    		<input type="hidden" name="ie" value="UTF-8" />
-			<input type="text" name="q" maxlength="36" size="17" placeholder="search our site"/>
-		</div>
-		<?php do_action( 'foundationpress_searchform_before_search_button' ); ?>
-		<div class="column small-12 medium-4 large-2">
-			<button type="submit" class="prefix button success" name="sa">
-				<i class="fa fa-search fa-large"></i>
-			</button>
-		</div>
-		<?php do_action( 'foundationpress_searchform_after_search_button' ); ?>
+	<div class="extend hide-for-small-only">
+  	<div class="row collapse">
+<?php do_action( 'foundationpress_searchform_top' ); ?>
+    	<div class="medium-5 medium-push-6 large-3 large-push-8 column">
+      	<input type="hidden" name="cx" value="009032756241832010141:tvv1cneufti" />
+		  	<input type="hidden" name="cof" value="FORID:10;NB:1" />
+		  	<input type="hidden" name="ie" value="UTF-8" />
+				<input type="text" name="q" maxlength="36" size="17" placeholder="search our site"/>
+    	</div>
+<?php do_action( 'foundationpress_searchform_before_search_button' ); ?>
+    	<div class="medium-1 large-1 column">
+      	<button type="submit" class="postfix button" name="sa">
+					<i class="fa fa-search fa-large"></i>
+				</button>
+    	</div>
+<?php do_action( 'foundationpress_searchform_after_search_button' ); ?>
+  	</div>
 	</div>
 </form>
-
 <?php do_action( 'foundationpress_after_searchform' ); ?>
-
-</div>
