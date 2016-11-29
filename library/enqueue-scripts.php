e6438176ee24e34a0afac47346bee0d4dcc12f0f
Dan Klotz <danferth@gmail.com>
2016-11-29 01:08:14 +0000

    edit to make menue bar color changing eaiser

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -17,7 +17,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	//wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Oswald:400,700|Source+Sans+Pro:400,300,700' );
 
 	// Enqueue the main Stylesheet.
-	wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/assets/stylesheets/foundation.css?v=2.0.0' );
+	wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/assets/stylesheets/foundation.css' );
 
 	// Deregister the jquery version bundled with WordPress.
 	wp_deregister_script( 'jquery' );
