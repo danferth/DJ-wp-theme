a0b0545dda5dcec0e02efa79815a8d14173ff071
tic_dev <dan@htslabs.com>
2017-02-02 22:27:11 +0000

    added version var to enqueue scripts

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -9,6 +9,8 @@
  * @subpackage FoundationPress
  * @since FoundationPress 1.0.0
  */
+//version for updates
+$version = "4.2.0";
 
 if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	function foundationpress_scripts() {
@@ -23,40 +25,40 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	wp_deregister_script( 'jquery' );
 
 	// Modernizr is used for polyfills and feature detection. Must be placed in header. (Not required).
-	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/javascript/vendor/modernizr.js', array(), '2.8.3', false );
+	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/javascript/vendor/modernizr.js', array(), $version, false);
 
 	// Fastclick removes the 300ms delay on click events in mobile environments. Must be placed in header. (Not required).
-	wp_enqueue_script( 'fastclick', get_template_directory_uri() . '/assets/javascript/vendor/fastclick.js', array(), '1.0.0', false );
+	wp_enqueue_script( 'fastclick', get_template_directory_uri() . '/assets/javascript/vendor/fastclick.js', array(), $version, false );
 
 	// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
 	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
 	
 	//JQ validate
-	wp_enqueue_script( 'validate', get_template_directory_uri() . '/assets/javascript/lib/jquery.validate.min.js', array('jquery'), '1.11.1', true );
+	wp_enqueue_script( 'validate', get_template_directory_uri() . '/assets/javascript/lib/jquery.validate.min.js', array('jquery'), $version, true );
 	
 	//JQ waypoints for scroll triggers
-	wp_enqueue_script( 'waypoints', get_template_directory_uri() . '/assets/javascript/lib/jquery.waypoints.min.js', array('jquery'), '4.0.0', true );
+	wp_enqueue_script( 'waypoints', get_template_directory_uri() . '/assets/javascript/lib/jquery.waypoints.min.js', array('jquery'), $version, true );
 	
 	//TweenMax for animations
-	wp_enqueue_script('TweenMax', get_template_directory_uri() . '/assets/javascript/lib/TweenMax.min.js', array(), '1.18.0', true );
+	wp_enqueue_script('TweenMax', get_template_directory_uri() . '/assets/javascript/lib/TweenMax.min.js', array(), $version, true );
 	
 	//sweetalert cause it's cool man
-	wp_enqueue_script('sweetalert', get_template_directory_uri() . '/assets/javascript/lib/sweetalert.min.js', array(), '1.0.0');
+	wp_enqueue_script('sweetalert', get_template_directory_uri() . '/assets/javascript/lib/sweetalert.min.js', array(), $version, false);
 	
 	//angularjs
-	wp_enqueue_script('angular', get_template_directory_uri() . '/assets/javascript/lib/angular.min.js', array(), '1.4', true);
+	wp_enqueue_script('angular', get_template_directory_uri() . '/assets/javascript/lib/angular.min.js', array(), $version, true);
 	//angular-sanitize
-	wp_enqueue_script('angular-sanitize', get_template_directory_uri() . '/assets/javascript/lib/angular-sanitize.min.js', array('angular'), '1.4', true);
+	wp_enqueue_script('angular-sanitize', get_template_directory_uri() . '/assets/javascript/lib/angular-sanitize.min.js', array('angular'), $version, true);
 	
 	//app.js (angular stuff goes here for now)
-	wp_enqueue_script('app', get_template_directory_uri(). '/assets/javascript/app.js', array('angular'), '2.0.0', true);
-	wp_enqueue_script('directives', get_template_directory_uri(). '/assets/javascript/directives.js', array('angular'), '2.0.0', true);
+	wp_enqueue_script('app', get_template_directory_uri(). '/assets/javascript/app.js', array('angular'), $version, true);
+	wp_enqueue_script('directives', get_template_directory_uri(). '/assets/javascript/directives.js', array('angular'), $version, true);
 	//site.js TIC custome js for pages
-	wp_enqueue_script('site', get_template_directory_uri() . '/assets/javascript/site.js', array('jquery'), '2.0.0', true);
+	wp_enqueue_script('site', get_template_directory_uri() . '/assets/javascript/site.js', array('jquery'), $version, true);
 
 	// If you'd like to cherry-pick the foundation components you need in your project, head over to Gruntfile.js and see lines 67-88.
 	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
-	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '5.5.2', true );
+	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), $version, true );
 
 	// Add the comment-reply library on pages where it is necessary
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
