a89307b72a4191af72e7e202e36bcb5befe85320
danferth <danferth@gmail.com>
2017-02-11 00:00:07 +0000

    removed var_dump

--- form-parse/parse-order-request.php
+++ form-parse/parse-order-request.php
@@ -3,8 +3,6 @@ require_once("PHPMailer/PHPMailerAutoload.php");
 date_default_timezone_set('America/Los_Angeles');
 header('HTTP/1.1 303 See Other');
 
-var_dump($_POST);
-
 //trim post
 function trim_value($value){
   if(gettype($value) == 'string'){
