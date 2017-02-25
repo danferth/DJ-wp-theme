609103cc80def6005149b011aff273f7e2e79574
danferth <danferth@gmail.com>
2017-02-25 00:27:47 +0000

    tweaked the inquary module for mobile

--- library/shortcodes.php
+++ library/shortcodes.php
@@ -345,7 +345,10 @@ function intro_quiry_shortcode($atts, $content=null, $tag){
 		    <h2>".$a['title']."</h2>\n
 		    ".$content."
 	    </div>\n
-	    <div class='small-12 medium-6 large-5 column'>\n
+	    <div class='no-padding small-12 show-for-small-only column'>\n
+		    <product-inquiry product='".$a['product']."'></product-inquiry>\n
+	    </div>\n
+	    <div class='medium-6 large-5 show-for-medium-up column'>\n
 		    <product-inquiry product='".$a['product']."'></product-inquiry>\n
 	    </div>\n
     </div>";
