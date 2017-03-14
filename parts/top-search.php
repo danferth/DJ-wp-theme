e5fbf5812f2b4ac3d2c7e7c4bdceb2a42ed9f61c
danferth <danferth@gmail.com>
2017-03-14 18:36:03 +0000

    fixed bug in google custom serach - this was also done on wp database as well

--- parts/top-search.php
+++ parts/top-search.php
@@ -15,8 +15,8 @@
 <?php do_action( 'foundationpress_searchform_top' ); ?>
     	<div class="medium-5 medium-push-6 large-3 large-push-8 column">
       	<input type="hidden" name="cx" value="009032756241832010141:tvv1cneufti" />
-		  	<input type="hidden" name="cof" value="FORID:10;NB:1" />
 		  	<input type="hidden" name="ie" value="UTF-8" />
+		  	<input type="hidden" name="cof" value="FORID:10;NB:1" />
 				<input type="text" name="q" maxlength="36" size="17" placeholder="search our site"/>
     	</div>
 <?php do_action( 'foundationpress_searchform_before_search_button' ); ?>
